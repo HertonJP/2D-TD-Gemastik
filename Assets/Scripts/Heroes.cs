ffbea527800bf776d28ecd0c36c615ec0f59eefa
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-06-24 10:00:07 +0700

    Flip Flop

--- Assets/Scripts/Heroes.cs
+++ Assets/Scripts/Heroes.cs
@@ -7,11 +7,17 @@ public class Heroes : MonoBehaviour
 {
     [SerializeField] private Transform rotationPoint;
     [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject heroObject;
 
     [SerializeField] private float targetingRange = 3f;
+    private SpriteRenderer heroSpriteRenderer;
 
     private Transform target;
 
+    private void Start()
+    {
+        heroSpriteRenderer = heroObject.GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
         if(target == null)
@@ -33,15 +39,21 @@ public class Heroes : MonoBehaviour
         Vector2 direction = target.position - transform.position;
         RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction, targetingRange, enemyMask);
 
+        bool enemyPassed = false;
+
         foreach (RaycastHit2D hit in hits)
         {
             if (hit.transform != target)
             {
-                // Flip the sprite horizontally
-                SpriteRenderer spriteRenderer = rotationPoint.GetComponent<SpriteRenderer>();
-                spriteRenderer.flipX = direction.x < 0;
+                enemyPassed = true;
                 break;
             }
+        }
+
+        // Flip the sprite based on the enemy passing
+        if (enemyPassed)
+        {
+            heroSpriteRenderer.flipX = direction.x < 0;
         }
     }
     private bool inRange()

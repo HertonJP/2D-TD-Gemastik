d076ff6132793555c44afa8087dbd885d32f302f
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-06-29 13:12:20 +0700

    Minor

--- Assets/Scripts/enemyHealth.cs
+++ Assets/Scripts/enemyHealth.cs
@@ -6,12 +6,15 @@ public class enemyHealth : MonoBehaviour
 {
     [SerializeField] private int HP = 2;
 
+
+    private bool isDestroyed = false;
     public void TakeDamage(int damage)
     {
         HP -= damage;
-        if(HP <= 0)
+        if(HP <= 0 && !isDestroyed)
         {
             Spawner.onEnemyDestroy.Invoke();
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }

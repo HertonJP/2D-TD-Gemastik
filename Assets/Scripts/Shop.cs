930c4ae302d65da0dc00d3f4fb892245da47404a
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-07-02 00:00:14 +0700

    Shop Update

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -6,6 +6,24 @@ using TMPro;
 public class Shop : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI currencyUI;
+    [SerializeField] private GameObject ShopPanel;
+
+    public void onClickPanel()
+    {
+        if(ShopPanel.activeSelf == true)
+        {
+            ShopPanel.SetActive(false);
+        }
+        else
+        {
+            ShopPanel.SetActive(true);
+        }
+    }
+
+    public void closePanel()
+    {
+        ShopPanel.SetActive(false);
+    }
     
 
     private void OnGUI()

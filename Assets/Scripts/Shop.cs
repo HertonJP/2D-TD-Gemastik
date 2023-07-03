75f0b0ce374aac803e8c3e19d8658a81f9a19f09
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-07-03 15:14:59 +0700

    Minor

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -7,37 +7,56 @@ public class Shop : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI currencyUI;
     [SerializeField] private GameObject ShopPanel;
-
+    private BuildManager buildManager;
+    private int selectedHeroIndex = -1;
 
     private void Start()
     {
+        buildManager = BuildManager.main;
         ShopPanel.SetActive(false);
     }
-    public void onClickPanel()
+
+    public void onClickPanel(int heroIndex)
     {
-        if(ShopPanel.activeSelf == true)
+        if (ShopPanel.activeSelf == true && selectedHeroIndex == heroIndex)
         {
             ShopPanel.SetActive(false);
+            selectedHeroIndex = -1;
         }
         else
         {
             ShopPanel.SetActive(true);
+            selectedHeroIndex = heroIndex;
         }
     }
 
     public void closePanel()
     {
         ShopPanel.SetActive(false);
+        selectedHeroIndex = -1;
     }
-    
 
     private void OnGUI()
     {
-       currencyUI.text = LevelManager.main.nutrition.ToString();
+        currencyUI.text = LevelManager.main.nutrition.ToString();
     }
 
     public void SetSelected()
     {
-
+        if (selectedHeroIndex != -1)
+        {
+            HeroTiles selectedHero = buildManager.GetHeroByIndex(selectedHeroIndex);
+            if (selectedHero != null)
+            {
+                GameObject heroPrefab = selectedHero.prefab;
+                if (heroPrefab != null)
+                {
+                    // Assuming you have a reference to the selected hero tile,
+                    // you can call the SpawnHero method in BuildManager to spawn the hero.
+                    // Pass the hero prefab and the selected hero tile as arguments.
+                    // buildManager.SpawnHero(heroPrefab, selectedHeroTile);
+                }
+            }
+        }
     }
 }
